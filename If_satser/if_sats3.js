bdd6d81bd75aee20b7e57bdfad9b8bde6fa128c8
Kokchun <69901510+kokchungiang@users.noreply.github.com>
2020-10-13 09:38:55 +0200

    funktioner genomgång lektion 1

--- If_satser/if_sats3.js
+++ If_satser/if_sats3.js
@@ -6,23 +6,24 @@ ungdom under 20 år
 pensionär över 65 år 
  */
 
-let age = 66
+let age = 66;
 //  eller: ||
-if (age < 20 || age > 65){
-    console.log("Det kostar 500kr")
+if (age < 20 || age > 65) {
+  console.log("Det kostar 500kr");
 } else {
-    console.log("775kr")
+  console.log("775kr");
 }
 
+// hej
 
 /* höjd > 130 och ålder > 10 så får man
 åka balder på liseberg */
 
-let alder = 12
-let hojd = 125
+let alder = 12;
+let hojd = 125;
 
-if (alder > 10 && hojd > 130){
-    console.log("Du får åka balder")
+if (alder > 10 && hojd > 130) {
+  console.log("Du får åka balder");
 } else {
-    console.log("Åk något annat")
+  console.log("Åk något annat");
 }
