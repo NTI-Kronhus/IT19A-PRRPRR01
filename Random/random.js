b3bfe9596019317a51c72e3c69c3b90d5f30a249
Kokchun <69901510+kokchungiang@users.noreply.github.com>
2020-12-03 09:58:07 +0100

    tärningskast

---
+++ Random/random.js
@@ -0,0 +1,41 @@
+console.log("Random hej")
+let slumptal = Math.random()
+
+console.log(slumptal)
+
+let coinflip = Math.round(slumptal)
+
+console.log(`Coin flip: ${coinflip}`)
+
+/* utöka det här programmet sådant att 
+    en 0 skriver ut tails
+    en 1 skriver ut head
+*/
+
+// använd en if-sats 
+if (coinflip == 1){
+    console.log("Head")
+} else{
+    console.log("Tails")
+}
+
+// tärning 
+/*  1. slå tärningen 100 gånger 
+    2. räkna antalet sexor
+*/ 
+let sexor = 0
+
+for (let i = 0; i < 1000000; i++){
+    let tärning = Math.floor(Math.random()*6)+1
+
+    if (tärning == 6){
+        sexor++;
+    }
+}
+
+console.log(`Antalet sexor är: ${sexor}`)
+
+let andel = sexor/1000000
+console.log(andel)
+console.log(`Teoretisk sannolikhet ${1/6}`)
+
